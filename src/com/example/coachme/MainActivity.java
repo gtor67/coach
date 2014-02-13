6a462bc50c822554315d1a2775b402ed18d7462f
Nazmul Hussain <al_rajja@yahoo.com>
2014-02-12 22:34:59 -0800

    not much new, linking button to next screen

--- src/com/example/coachme/MainActivity.java
+++ src/com/example/coachme/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.coachme;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +22,9 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    /** Called when the user clicks the Baseball button */
+    public void chooseBaseball(View view) {
+    	Intent intent = new Intent(this, TrainingFilterActivity.class);
+    	startActivity(intent);
+    }
 }
