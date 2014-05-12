415d2f17621fbb4148673e924535481f2a87d829
razikx <razikx6@gmail.com>
2014-05-12 11:03:59 -0700

    minor bug fix in favs page, list view wont expand even if you have multiple favorites saved

--- src/com/example/coachme/LoginActivity.java
+++ src/com/example/coachme/LoginActivity.java
@@ -405,27 +405,7 @@ public class LoginActivity extends Activity {
 					    	if(user.getBoolean("emailVerified")){
 					    		
 				////////////////////////FIX SUBCRIBE BUG
-					    		
-					    		
-//					    		ParseQuery<ParseObject> innerQuery = ParseQuery.getQuery("players");
-//					    		innerQuery.whereExists(user.getEmail());
-//					    		ParseQuery<ParseObject> query = ParseQuery.getQuery("coaches");
-//					    		query.whereMatchesQuery("player", innerQuery);
-//					    		query.findInBackground(new FindCallback<ParseObject>() {
-//					    		  public void done(List<ParseObject> playerList, ParseException e) {
-//					    		    // list of players 
-//					    			  if(playerList !=null){
-//					    				  Log.d("add", "already part of team");
-//										    //Register a channel to test push channels
-//										      Context ctx = LoginActivity.this.getApplicationContext();
-//										        PushService.subscribe(ctx, teamTitle, PushResponse.class);
-//					    				  
-//					    			  }
-//					    		  }
-//					    		});
-					    		
-					    		
-					    		
+	    			
 					    		ParseQuery<ParseObject> query =ParseQuery.getQuery("coaches");
 					    		query.orderByAscending("createdAt");
 								query.findInBackground(new FindCallback<ParseObject>() {
@@ -434,10 +414,7 @@ public class LoginActivity extends Activity {
 										if (coach == null) {
 										} else {
 											for(int i= 0; i < coach.size(); i++){
-											final String teamTitle = coach.get(i).getString("name");
-//											ParseInstallation pi = ParseInstallation.getCurrentInstallation();
-//									        
-//									        pi.saveEventually();
+											final String teamTitle = coach.get(i).getString("name");											
 									       
 											//check if player is already part of the team
 											ParseRelation<ParseObject> relation = coach.get(i).getRelation("players");
@@ -460,17 +437,8 @@ public class LoginActivity extends Activity {
 										}
 									}
 								});
-//					    		
-					    		
-					    		
-					  
-				/////////////////////////END SUBSCRIBE BUG FIX
-					    		
-					    		
-					    		
-					    		
-					    		
-					    		
+				  
+				/////////////////////////END SUBSCRIBE BUG FIX  		
 					    		
 					    		test = true; 
 					    	}
@@ -490,29 +458,7 @@ public class LoginActivity extends Activity {
 			} catch (InterruptedException e) {
 				return false;
 			}
-/////// testing parse log in ///////////////////////////
-		/**	
-			ParseUser user = new ParseUser();
-			user.setUsername("Jerry@jerry.com");
-			user.setPassword("showmethemoney");
-			user.setEmail("email@example.com");
-			 
-			// other fields can be set just like with ParseObject
-			//user.put("phone", "650-253-0000");
-			 
-			user.signUpInBackground(new SignUpCallback() {
-			  public void done(ParseException e) {
-			    if (e == null) {
-			      // Hooray! Let them use the app now.
-			    } else {
-			      // Sign up didn't succeed. Look at the ParseException
-			      // to figure out what went wrong
-			    }
-			  }
-			});
-			*/
-			
-	////////////////end parse login test  ///////////////////////
+
 			return test;
 		}
 
